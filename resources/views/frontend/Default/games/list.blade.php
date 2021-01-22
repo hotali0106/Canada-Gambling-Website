7282d16dc661d5edf1f6c82c3b53ab2dd50b8d19
applewood524 <maxteamapplewood@outlook.com>
2021-01-23 01:18:04 +0900

    fix: resolve conflicts

--- resources/views/frontend/Default/games/list.blade.php
+++ resources/views/frontend/Default/games/list.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.Default.layouts.app')
 @section('slider')
 <section id="hero-section">
-    <iframe style="overflow:hidden !important; height:370px; padding:0px !important; margin:0px !important; border: none !important;" width="100%" src="https://canada777.com/slider" allowfullscreen scrolling="no"></iframe>
+    <iframe style="overflow:hidden !important; height:370px; padding:0px !important; margin:0px !important; border: none !important;" width="100%" src="http://canada777.com/slides/slide.php" allowfullscreen scrolling="no"></iframe>
 </section>
 @endsection
 @section('content')
