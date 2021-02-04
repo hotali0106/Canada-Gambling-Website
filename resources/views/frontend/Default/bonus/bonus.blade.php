f99344f30bb9fb644dd3b78887f4cae51f34aede
hotali7 <hotali0106@gmail.com>
2021-02-04 17:22:13 +1000

    fix: update the size of image on bonus page

--- resources/views/frontend/Default/bonus/bonus.blade.php
+++ resources/views/frontend/Default/bonus/bonus.blade.php
@@ -14,8 +14,14 @@
             <img class="hero-image" src="{{asset('frontend/Page/image/bonus-banner.jpg')}}" />
         </section>
         <section id="bonus-section">
-            <div class="position-relative bonus-content py-5 px-5">
-                <img class="section-image" src="{{asset('frontend/Page/image/bonus-content-image.jpg')}}" />
+            <div class="position-relative bonus-content py-5 px-5 d-flex justify-content-center">
+                @if(Auth::check())
+                    <img class="section-image" src="{{asset('frontend/Page/image/bonus-content-image.jpg')}}" />
+                @else
+                    <a href="#signin-modal" class="d-flex justify-content-center">
+                        <img class="section-image" src="{{asset('frontend/Page/image/bonus-content-image.jpg')}}" />
+                    </a>
+                @endif
                 <a href="#" class="position-absolute d-block text-light" style="bottom: 10px; right: 100px;">Terms Apply</a>
             </div>
             <p class="text-center text-light py-2 px-2 border-top border-default mb-0">1st Deposit - Match Bonus up to C$ 400 • 2nd / 3rd Deposit - Match Bonus up to C$ 300 • New customers only • Min deposit C$ 10 • 70x wagering</p>
